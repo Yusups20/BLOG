a77a43ebf9d108cffba1a947b995e3c26cc5424f
Yusups20 <yups2092003@gmail.com>
2019-11-25 11:39:57 +0700

    Template Blog

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<!-- Header -->
+ <?php include('../layouts/includes/head.php') ?>
+<!-- End Header -->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <!-- Navbar -->
+    <?php include('../layouts/includes/navbar.php') ?>
+    <!-- End Navbar -->
+    <div class="app-body">
+      <!-- Sidebar -->
+      <?php include('../layouts/includes/sidebar.php') ?>
+      <!-- End Sidebar -->
+      <!-- Main Content -->
+      <main class="main">
+        <center>HALLO WORLD</center>
+      </main>
+      <!-- End Main Content -->
+      
+    </div>
+    <footer class="app-footer">
+      <div>
+        <a href="https://coreui.io">CoreUI</a>
+        <span>&copy; 2018 creativeLabs.</span>
+      </div>
+      <div class="ml-auto">
+        <span>Powered by</span>
+        <a href="https://coreui.io">CoreUI</a>
+      </div>
+    </footer>
+    <!-- CoreUI and necessary plugins-->
+    <?php include('../layouts/includes/script.php') ?>
+  </body>
+</html>
